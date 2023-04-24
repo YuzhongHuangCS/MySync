c71dffc795818d5645ba7ad5d05d9e9a1c59e494
Yuzhong Huang <yuzhongh@usc.edu>
2023-04-24 01:14:45 -0700

    basic DataGridView

---
+++ MainForm.Designer.cs
@@ -0,0 +1,161 @@
+﻿using System.Windows.Forms;
+
+namespace MyUpload {
+    partial class MainForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.songsDataGridView = new System.Windows.Forms.DataGridView();
+            this.buttonPanel = new System.Windows.Forms.Panel();
+            this.deleteRowButton = new System.Windows.Forms.Button();
+            this.addNewRowButton = new System.Windows.Forms.Button();
+            this.ReleaseDate = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Track = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Title = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Artist = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Album = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.songsDataGridView)).BeginInit();
+            this.buttonPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // songsDataGridView
+            // 
+            this.songsDataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.songsDataGridView.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCellsExceptHeaders;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.songsDataGridView.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.songsDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.songsDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ReleaseDate,
+            this.Track,
+            this.Title,
+            this.Artist,
+            this.Album});
+            this.songsDataGridView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.songsDataGridView.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.songsDataGridView.Location = new System.Drawing.Point(0, 0);
+            this.songsDataGridView.MultiSelect = false;
+            this.songsDataGridView.Name = "songsDataGridView";
+            this.songsDataGridView.RowHeadersVisible = false;
+            this.songsDataGridView.RowTemplate.DefaultCellStyle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.songsDataGridView.RowTemplate.Height = 24;
+            this.songsDataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.songsDataGridView.Size = new System.Drawing.Size(942, 553);
+            this.songsDataGridView.TabIndex = 0;
+            // 
+            // buttonPanel
+            // 
+            this.buttonPanel.Controls.Add(this.deleteRowButton);
+            this.buttonPanel.Controls.Add(this.addNewRowButton);
+            this.buttonPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.buttonPanel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonPanel.Location = new System.Drawing.Point(0, 507);
+            this.buttonPanel.Name = "buttonPanel";
+            this.buttonPanel.Size = new System.Drawing.Size(942, 46);
+            this.buttonPanel.TabIndex = 1;
+            // 
+            // deleteRowButton
+            // 
+            this.deleteRowButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.deleteRowButton.Location = new System.Drawing.Point(149, 3);
+            this.deleteRowButton.Name = "deleteRowButton";
+            this.deleteRowButton.Size = new System.Drawing.Size(148, 40);
+            this.deleteRowButton.TabIndex = 1;
+            this.deleteRowButton.Text = "Delete Row";
+            this.deleteRowButton.UseVisualStyleBackColor = true;
+            this.deleteRowButton.Click += new System.EventHandler(this.deleteRowButton_Click);
+            // 
+            // addNewRowButton
+            // 
+            this.addNewRowButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.addNewRowButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addNewRowButton.Location = new System.Drawing.Point(3, 3);
+            this.addNewRowButton.Name = "addNewRowButton";
+            this.addNewRowButton.Size = new System.Drawing.Size(140, 40);
+            this.addNewRowButton.TabIndex = 0;
+            this.addNewRowButton.Text = "Add Row";
+            this.addNewRowButton.UseVisualStyleBackColor = true;
+            this.addNewRowButton.Click += new System.EventHandler(this.addNewRowButton_Click);
+            // 
+            // ReleaseDate
+            // 
+            this.ReleaseDate.HeaderText = "Release Date";
+            this.ReleaseDate.Name = "ReleaseDate";
+            // 
+            // Track
+            // 
+            this.Track.HeaderText = "Track";
+            this.Track.Name = "Track";
+            // 
+            // Title
+            // 
+            this.Title.HeaderText = "Title";
+            this.Title.Name = "Title";
+            // 
+            // Artist
+            // 
+            this.Artist.HeaderText = "Artist";
+            this.Artist.Name = "Artist";
+            // 
+            // Album
+            // 
+            this.Album.HeaderText = "Album";
+            this.Album.Name = "Album";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(942, 553);
+            this.Controls.Add(this.buttonPanel);
+            this.Controls.Add(this.songsDataGridView);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            ((System.ComponentModel.ISupportInitialize)(this.songsDataGridView)).EndInit();
+            this.buttonPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DataGridView songsDataGridView;
+        private Panel buttonPanel;
+        private Button addNewRowButton;
+        private Button deleteRowButton;
+        private DataGridViewTextBoxColumn ReleaseDate;
+        private DataGridViewTextBoxColumn Track;
+        private DataGridViewTextBoxColumn Title;
+        private DataGridViewTextBoxColumn Artist;
+        private DataGridViewTextBoxColumn Album;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyUpload {
+    public partial class MainForm : Form {
+        public MainForm() {
+            InitializeComponent();
+            PopulateDataGridView();
+            songsDataGridView.CellMouseMove += new DataGridViewCellMouseEventHandler(songsDataGridView_CellMouseMove);
+        }
+
+        private void songsDataGridView_CellMouseMove(object sender, DataGridViewCellMouseEventArgs e) {
+            songsDataGridView.ClearSelection();
+            if (e.RowIndex > -1) {
+                songsDataGridView.Rows[e.RowIndex].Selected = true;
+            }
+        }
+        private void addNewRowButton_Click(object sender, EventArgs e) {
+            this.songsDataGridView.Rows.Add();
+        }
+
+        private void deleteRowButton_Click(object sender, EventArgs e) {
+            if (this.songsDataGridView.SelectedRows.Count > 0 &&
+                this.songsDataGridView.SelectedRows[0].Index !=
+                this.songsDataGridView.Rows.Count - 1) {
+                this.songsDataGridView.Rows.RemoveAt(
+                    this.songsDataGridView.SelectedRows[0].Index);
+            }
+        }
+
+        private void PopulateDataGridView() {
+            string[] row0 = { "11/22/1968", "29", "Revolution 9",
+            "Beatles", "The Beatles [White Album]" };
+            string[] row1 = { "1960", "6", "Fools Rush In",
+            "Frank Sinatra", "Nice 'N' Easy" };
+            string[] row2 = { "11/11/1971", "1", "One of These Days",
+            "Pink Floyd", "Meddle" };
+            string[] row3 = { "1988", "7", "Where Is My Mind?",
+            "Pixies", "Surfer Rosa" };
+            string[] row4 = { "5/1981", "9", "Can't Find My Mind",
+            "Cramps", "Psychedelic Jungle" };
+            string[] row5 = { "6/10/2003", "13",
+            "Scatterbrain. (As Dead As Leaves.)",
+            "Radiohead", "Hail to the Thief" };
+            string[] row6 = { "6/30/1992", "3", "Dress", "P J Harvey", "Dry" };
+
+            songsDataGridView.Rows.Add(row0);
+            songsDataGridView.Rows.Add(row1);
+            songsDataGridView.Rows.Add(row2);
+            songsDataGridView.Rows.Add(row3);
+            songsDataGridView.Rows.Add(row4);
+            songsDataGridView.Rows.Add(row5);
+            songsDataGridView.Rows.Add(row6);
+
+            songsDataGridView.Columns[0].DisplayIndex = 3;
+            songsDataGridView.Columns[1].DisplayIndex = 4;
+            songsDataGridView.Columns[2].DisplayIndex = 0;
+            songsDataGridView.Columns[3].DisplayIndex = 1;
+            songsDataGridView.Columns[4].DisplayIndex = 2;
+        }
+    }
+}
